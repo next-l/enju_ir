b410193ee51c3746785b628652d76fe3266b7068
Kosuke Tanabe <nabeta@fastmail.fm>
2014-09-03 00:53:17 +0900

    updated link to fedora

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,5 +1,5 @@
 module ResourcesHelper
   def link_to_attachment(resource)
-    link_to 'download', "/fedora/objects/#{resource.id}/datastreams/attachment/content" if resource.attachment.content
+    link_to 'download', "#{Setting.fedora.base_url}/objects/#{resource.id}/datastreams/attachment/content" if resource.attachment.content
   end
 end
