c5e3e0110bfdf13b4a2519bac1b606938f1d16cf
Kosuke Tanabe <nabeta@fastmail.fm>
2021-02-15 03:48:11 +0900

    add attributes to DataciteSerializer

--- app/serializers/enju_ir/datacite_serializer.rb
+++ app/serializers/enju_ir/datacite_serializer.rb
@@ -1,8 +1,9 @@
 module EnjuIr
   class DataciteSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :event, :doi, :creators, :titles, :publisher, :publicationYear, :types, :url, :schema_version
+    attributes :event, :doi, :creators, :titles, :publisher, :publicationYear,
+      :types, :url, :schema_version
     set_type :dois
-    set_key_transform :camel_lower
+    # set_key_transform :camel_lower
   end
 end
