7afe29cc6b065b632aaa94e97b863c97e5176f5c
Kosuke Tanabe <nabeta@fastmail.fm>
2021-04-28 22:19:58 +0900

    set default value for url attribute

--- app/serializers/enju_ir/datacite_serializer.rb
+++ app/serializers/enju_ir/datacite_serializer.rb
@@ -1,8 +1,12 @@
 module EnjuIr
   class DataciteSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :event, :doi, :creators, :titles, :publisher, :publicationYear, :types, :url, :schema_version
+    attributes :event, :doi, :creators, :titles, :publisher, :publicationYear, :types, :schema_version
     set_type :dois
     set_key_transform :camel_lower
+
+    attribute :url do |object|
+      EnjuIr::Engine.routes.url_helpers.dataset_url(object.id)
+    end
   end
 end
